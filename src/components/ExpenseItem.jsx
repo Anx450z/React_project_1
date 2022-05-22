9652d34ba42f9463c687de4355e17089b5288653
Anx450z <anx450z@gmail.com>
2022-05-22 23:08:32 +0530

    Initial commit

---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,15 @@
+// component in react is a function
+function ExpenseItem(){
+  return (
+    <div>
+      <div>Date</div>
+      <div>
+        <h2>Title</h2>
+        <div>Amount</div>
+      </div>
+    </div>
+  )
+    
+}
+
+export default ExpenseItem;

9cab2d83c059dc22772639553769b9c188316bcc
Anx450z <anx450z@gmail.com>
2022-05-22 23:23:42 +0530

    Add styling ExpenseItem.css

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div >
         <p>
+          Expense Item list
           <ExpenseItem></ExpenseItem>
         </p>
     </div>
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,14 +1,22 @@
 // component in react is a function
+
+import './ExpenseItem.css';
+
 function ExpenseItem(){
+
+  const expenseDate = new Date(2022, 2, 28); //regular javascript code
+  const expenseTitle = 'Car Insurance'
+  const expenseAmount = 550.33
+  
   return (
-    <div>
-      <div>Date</div>
-      <div>
-        <h2>Title</h2>
-        <div>Amount</div>
+    <div className='expense-item'>
+      <div>{expenseDate.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
       </div>
     </div>
-  )
+  );
     
 }
 

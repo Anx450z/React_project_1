ccb1529605568fcaffc09e8557db55390b145ce7
Anx450z <anx450z@gmail.com>
2022-05-24 22:56:49 +0530

    Add button and a hook to update title

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,17 +1,26 @@
-// component in react is a function
+import React, { useState } from 'react';
 
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+
+  const [title, setTitle]= useState(props.title); //react Hook, starts with use..., return array with 2 values
+
+  const clickHandeler = () => {
+    setTitle('Updated');
+    console.log('Clicked!!');
+  };
+  
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandeler}>Change Title</button> 
     </Card>
   );  
 }
